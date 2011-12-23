7d3498a20e31d18e77c7bf90cdeef8e4591ba8e0
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-12-23 09:11:46 +0000

    a bit of manual GC

--- src/bink/look.clj
+++ src/bink/look.clj
@@ -26,26 +26,35 @@
 		      
 	       ;; define the draw function
 	       (draw []
-
+		     
 		     (dosync
+		      
+					; remove balls that aren't onscreen any more
+		      (ref-set balls (filter #(let [[x y _ _] @%] (and (< 0 x w) (< 0 y h))) @balls))
+
+		      
+					; update all positions
 		      (doseq [b @balls]
 			(let [[x y dx dy] @b]
 			  (ref-set b [(+ dx x) (+ dy y) dx (+ grav dy)]))))
 
+		     
+					; clear background
 		     (doto this
 		       (background 0)
 		       (fill 0)
 		       (stroke 255 0 0))		     
-		     
+
+					; draw balls
 		     (doseq [b @balls]
 		       (let [[x y _ _] @b]
 			 (doto this
 			   (ellipse x y 5 5)))))
 	       
-	       (mouseClicked [e]
+	       (mousePressed [e]
 			     (dosync
 			      (ref-set balls (conj @balls (new-ball (mouse-x e)
-								   (mouse-y e)))))))]
+								    (mouse-y e)))))))]
     
     ;; use the view function to display the sketch
     (view sktch :title "BINK" :size [w h])))
